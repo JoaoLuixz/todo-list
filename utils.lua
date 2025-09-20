7ca35521bb1e80a646d720d94f97c663302cc1b9
João Luiz <joaoluizmartins6@gmail.com>
2025-09-20 13:49:15 -0300

    add `addTodo` function

--- utils.lua
+++ utils.lua
@@ -1,14 +1,27 @@
 local utils = {};
 
 function utils.createNecessaryFiles()
-  local file = io.open(".\\db\\todos.json", "w");
+  local db = io.open(".\\db\\todos.json", "w");
 
   if not file then
     os.execute("mkdir db");
-    os.execute("type nul > .\\db\\todos.json")
   end
-    print("Files created!")
+
+  db = io.open(".\\db\\todos.json", "w");
+  db:write("[]");
+  db:close();
+  print("Files created!")
 end
 
-
+function utils.addTodo(content)
+  local db = io.open(".\\db\\todos.json", "r");
+  local allDbContent = db:read("*a");
+  print(allDbContent)
+  local formattedContent = string.format("{\"content\":\"%s\", \"status\":false},", content);
+  db:close();
+  db = io.open(".\\db\\todos.json", "w");
+  print(formattedContent);
+  db:write(string.format("%s %s %s", string.sub(allDbContent, 1, #allDbContent - 1), formattedContent, string.sub(allDbContent, -1) ))
+  db:close()
+end
 return utils
